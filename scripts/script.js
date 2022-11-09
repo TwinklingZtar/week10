9bd5aba4ffbde9596ccef41cbd589c442f49075d
Ian <istewart17@my.bcit.ca>
2022-11-09 14:10:44 -0800

    practice for-of loops

--- scripts/script.js
+++ scripts/script.js
@@ -24,29 +24,54 @@
 // console.log(user_name)
 // alert("Hello " + user_name)
 // alert(`Hello ${user_name}`)
-let fruits = ["Apple", "Banana"];
-let newfruits = fruits.push("Orange");
+// let fruits = ["Apple", "Banana"];
+// let newfruits = fruits.push("Orange");
 // console.log(newfruits)
 // let poppedfruit = fruits.pop();
 // console.log(poppedfruit)
-fruits.unshift("Mango");
+// fruits.unshift("Mango");
 // fruits.shift()
 
-for (let i = 0; i < fruits.length; i++){
-    console.log(fruits[i])
+// for (let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// };
+
+// for (let item of fruits){
+//     console.log(item)
+// };
+
+
+// const person = {
+//     name:['Bob', "Smith"],
+//     age:32,
+//     gender:"male",
+//     interests: ["Music", "Skiing"]
+// }
+
+// console.log(`My name is ${person.name[0]} ${person.name[1]}. I am ${person.age} 
+// years old. I am into ${person.interests[0]} and ${person.interests[1]}`)
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
 };
 
-for (let item of fruits){
-    console.log(item)
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
 };
 
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3];
 
-const person = {
-    name:['Bob', "Smith"],
-    age:32,
-    gender:"male",
-    interests: ["Music", "Skiing"]
+for (let student of students){
+    if (student.program === "CIT" && student.GPA > 3){
+        console.log(student)
+    }
 }
-
-console.log(`My name is ${person.name[0]} ${person.name[1]}. I am ${person.age} 
-years old. I am into ${person.interests[0]} and ${person.interests[1]}`)
