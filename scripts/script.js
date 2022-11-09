dd27d847fdc54c9b602fdb9e0444b79926c410c4
Ian <istewart17@my.bcit.ca>
2022-11-09 13:31:41 -0800

    practice with do while loops

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
-console.log("hello")
+// console.log("hello")
 
-for (let i = 1; i<=10; i++){
-    console.log(2**i)
-}
+// for (let i = 1; i<=10; i++){
+//     console.log(2**i)
+// }
 
-let pwr = 1
-while (pwr<=10){
-    console.log(2**pwr)
-    pwr += 1
+// let pwr = 1
+// while (pwr<=10){
+//     console.log(2**pwr)
+//     pwr += 1
+// }
+
+// do {
+
+// }
+
+// while()
+let user_name
+do{
+    user_name = prompt("what is your name")
 }
+while (isNaN(Number(user_name)) != true)
+
+console.log(user_name)
+alert("Hello " + user_name)
+alert(`Hello ${user_name}`)
