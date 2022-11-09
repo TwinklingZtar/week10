7f29ae6e9724c5775e9425f5b40272c938042eb8
Ian <istewart17@my.bcit.ca>
2022-11-09 14:01:06 -0800

    object practice

--- scripts/script.js
+++ scripts/script.js
@@ -14,13 +14,39 @@
 
 // }
 
-// while()
-let user_name
-do{
-    user_name = prompt("what is your name")
-}
-while (isNaN(Number(user_name)) != true)
+// // while()
+// let user_name
+// do{
+//     user_name = prompt("what is your name")
+// }
+// while (isNaN(Number(user_name)) != true)
 
-console.log(user_name)
-alert("Hello " + user_name)
-alert(`Hello ${user_name}`)
+// console.log(user_name)
+// alert("Hello " + user_name)
+// alert(`Hello ${user_name}`)
+let fruits = ["Apple", "Banana"];
+let newfruits = fruits.push("Orange");
+// console.log(newfruits)
+// let poppedfruit = fruits.pop();
+// console.log(poppedfruit)
+fruits.unshift("Mango");
+// fruits.shift()
+
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i])
+};
+
+for (let item of fruits){
+    console.log(item)
+};
+
+
+const person = {
+    name:['Bob', "Smith"],
+    age:32,
+    gender:"male",
+    interests: ["Music", "Skiing"]
+}
+
+console.log(`My name is ${person.name[0]} ${person.name[1]}. I am ${person.age} 
+years old. I am into ${person.interests[0]} and ${person.interests[1]}`)
